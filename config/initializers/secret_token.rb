d0c788eef8ef101af8f62016d153b5ddc50ee39d
Tozkoparan <cetintozkoparan@gmail.com>
2013-01-30 17:42:51 +0200

    Fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '65ba3287db4660839ca87c3c72cbfc57e0b5539baf572c896767ce647e8d0c919c3d9d7fba9f1dc6a45fc3dd74297cf9bb0931fcc7a5b281be3fc32959d992b2'
